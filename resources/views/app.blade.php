9ec58fe71081a86542cad81077a864c716a73742
Rifqi <f8goodspoof@gmail.com>
2026-08-11 00:22:32 +0700

    fix delay

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,90 @@
     <head>
         <meta charset="utf-8">
 
+        @php
+            $pageScripts = [];
+            $pageStyles = [];
+            $manifestPath = public_path('build/manifest.json');
+            $pageComponent = data_get($page ?? [], 'component');
+
+            if (
+                is_string($pageComponent)
+                && $pageComponent !== ''
+                && is_file($manifestPath)
+            ) {
+                $manifest = json_decode(
+                    file_get_contents($manifestPath),
+                    true,
+                );
+
+                $manifestKey = "resources/js/Pages/{$pageComponent}.tsx";
+
+                if (
+                    is_array($manifest)
+                    && isset($manifest[$manifestKey])
+                    && is_array($manifest[$manifestKey])
+                ) {
+                    $seen = [];
+
+                    $collectChunk = function (string $key) use (
+                        &$collectChunk,
+                        &$pageScripts,
+                        &$pageStyles,
+                        &$seen,
+                        $manifest,
+                    ): void {
+                        if (
+                            isset($seen[$key])
+                            || !isset($manifest[$key])
+                            || !is_array($manifest[$key])
+                        ) {
+                            return;
+                        }
+
+                        $seen[$key] = true;
+                        $chunk = $manifest[$key];
+
+                        foreach ($chunk['imports'] ?? [] as $import) {
+                            if (is_string($import)) {
+                                $collectChunk($import);
+                            }
+                        }
+
+                        $file = $chunk['file'] ?? null;
+
+                        if (is_string($file) && $file !== '') {
+                            $pageScripts[] = $file;
+                        }
+
+                        foreach ($chunk['css'] ?? [] as $style) {
+                            if (is_string($style) && $style !== '') {
+                                $pageStyles[] = $style;
+                            }
+                        }
+                    };
+
+                    $collectChunk($manifestKey);
+                }
+            }
+
+            $pageScripts = array_values(array_unique($pageScripts));
+            $pageStyles = array_values(array_unique($pageStyles));
+        @endphp
+
+        @foreach ($pageStyles as $pageStyle)
+            <link
+                rel="stylesheet"
+                href="{{ asset('build/'.$pageStyle) }}"
+            >
+        @endforeach
+
+        @foreach ($pageScripts as $pageScript)
+            <link
+                rel="modulepreload"
+                href="{{ asset('build/'.$pageScript) }}"
+            >
+        @endforeach
+
         <link
             rel="preconnect"
             href="https://fonts.googleapis.com"
